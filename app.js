bb91cc409a0e5ba609681e38a3884379f386bae9
fahri muham <162775830+fahrimuham@users.noreply.github.com>
2024-05-19 22:04:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/*
+ Credits By KiiCode Projects 
+ Telegram : @kiicode
+ Channel WhatsApp : https://whatsapp.com/channel/0029VaZSdai5Ui2TMoNsYo0J
+ KiiCode Api : api.kiicodeit.me
+ Elxyz Api : api.elxyz.me
+*/
+
 const express = require('express');
 const { tiktok } = require('./tiktok');
 const path = require('path');
